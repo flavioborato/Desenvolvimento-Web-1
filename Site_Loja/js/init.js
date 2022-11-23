9af3f2f91e059e40d1ebcbd5c183715edaf5bd22
Flavio Fernando Borato <toraxico@msn.com>
2022-11-23 16:04:10 -0300

    Atualização
    Finalização do Site, executando JavaScript

--- Site_Loja/js/init.js
+++ Site_Loja/js/init.js
@@ -1,3 +1,6 @@
+
+
+
 (function($){
   $(function(){
 
@@ -6,3 +9,17 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var id_nome = document.getElementById('nome_id');
+
+$('#bt1').click(function(){
+  alert('Cliente Cadastrado com sucesso!!!');
+  id_nome.value = "sdsd "
+  id_nome.focus()
+});
+
+function cadastar(){
+
+  id_nome.value = "sdsd "
+  id_nome.focus()
+};
