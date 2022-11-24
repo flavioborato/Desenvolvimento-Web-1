6be2aa56d077bdc1c78b9c488f7bdfb846dcf5cb
Flavio Fernando Borato <toraxico@msn.com>
2022-11-24 13:46:36 -0300

    Finalização CSS
    Alterações no CSS do cadastro

--- Site_Loja/js/init.js
+++ Site_Loja/js/init.js
@@ -1,19 +1,8 @@
-
+/* JS alterações
+Por:- Flavio Fernando Borato */
 var nome = document.getElementById('nome_id');
 var telefone = document.getElementById('telef_id');
 var email = document.getElementById('email_id');
-
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-
-
 $('#bt1').click(function(){
   alert('Cliente Cadastrado com sucesso!!!');
 });
@@ -26,3 +15,23 @@ function cadastar(){
   email.value = ""
   email.focus()
 };
+
+/**
+ * Use this file to override Materialize files so you can update
+ * the core Materialize files in the future
+ *
+ * Made By MaterializeCSS.com
+ */
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+
