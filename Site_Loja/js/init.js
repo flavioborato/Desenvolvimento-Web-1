500f019811e45806653a4c74b2ca9765b9fb87c2
Flavio Fernando Borato <toraxico@msn.com>
2022-11-24 02:52:29 -0300

    Projeto Final 1
    Projeto finalizado para entrega

--- Site_Loja/js/init.js
+++ Site_Loja/js/init.js
@@ -1,5 +1,7 @@
 
-
+var nome = document.getElementById('nome_id');
+var telefone = document.getElementById('telef_id');
+var email = document.getElementById('email_id');
 
 (function($){
   $(function(){
@@ -10,16 +12,17 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-var id_nome = document.getElementById('nome_id');
+
 
 $('#bt1').click(function(){
   alert('Cliente Cadastrado com sucesso!!!');
-  id_nome.value = "sdsd "
-  id_nome.focus()
 });
 
 function cadastar(){
-
-  id_nome.value = "sdsd "
-  id_nome.focus()
+  nome.value = ""
+  nome.focus()
+  telefone.value = ""
+  telefone.focus()
+  email.value = ""
+  email.focus()
 };
